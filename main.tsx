e413813175a6b44ebd23c95cd2c6d247de32cb54
Pavel Ivanov <pavel.and.ivanov@yandex.ru>
2025-04-09 15:44:15 +0300

    add husky

--- main.tsx
+++ main.tsx
@@ -1,13 +1,12 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './src/app/App.tsx';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import './src/app/styles/index.scss';
-
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './src/app/App.tsx'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import './src/app/styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App/>
-    </StrictMode>,
-);
+        <App />
+    </StrictMode>
+)
