66a258ed86272cb3b6d95990373421661a9bf6e2
Pavel Ivanov <pavel.and.ivanov@yandex.ru>
2025-05-14 16:46:42 +0300

    fix journey sections

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import { Outlet } from 'react-router'
 import { Footer } from 'widgets/footer'
 import { Header } from 'widgets/header'
-import { JourneySection } from 'widgets/journey_section'
+import { StartJourney } from 'widgets/start_journey'
 import IconSprite from 'shared/assets/sprite.svg?react'
 import { footerLinks } from './lib/footerLinks'
 import { routes } from './lib/routes'
@@ -17,7 +17,7 @@ export const Layout: FC<Props> = () => {
             <main>
                 <Outlet />
             </main>
-            <JourneySection />
+            <StartJourney />
             <Footer links={footerLinks} />
         </>
     )
--- src/widgets/start_journey/index.ts
+++ src/widgets/start_journey/index.ts
@@ -1,3 +1,3 @@
-export { JourneySection } from './ui/JourneySection'
+export { StartJourney } from './ui/StartJourney.tsx'
 export { enJourney } from './i18n/en'
 export { ruJourney } from './i18n/ru'
